82adfcc61146dea86398dab9ed28f88d7d494144
Jeemin Shim <jeemin.shim@gmail.com>
2019-12-28 02:36:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,5 @@
 def square_array(array)
   squared = []
-  count = 0
-  while count < array.size do
-    squared << (array[count] ** 2)
-    count = count + 1
-  end
+  array.each { |element| squared << element ** 2 }
   squared
 end
